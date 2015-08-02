00faf9bc9df968404f0e32e4c8d1d591934f72f9
Andrew Lawson <aeriklawson@gmail.com>
2015-08-01 21:03:40 -0400

    Initial Commit

---
+++ VoxelEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VoxelEngine
+//
+//  Created by Andrew Lawson on 8/1/15.
+//  Copyright (c) 2015 Andrew Lawson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
